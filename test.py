3e7f87e0f8d3669c41cf1090b31f939717ba926c
MohammadYSF <aa.yosefiyan7@gmail.com>
2022-11-11 18:48:36 +0330

    finishing the test project
    I think I got the hang of the modules . now let's go the work on the real project

--- test.py
+++ test.py
@@ -3,10 +3,19 @@ import subprocess
 import argparse
 import time
 parser = argparse.ArgumentParser()
-parser.add_argument("time",type=int)
+parser.add_argument("time",type=int,help="specify the time in seconds")
 args = parser.parse_args()
-print(f"starting timer of {args.time} seconds")
-for _ in range(args.time):
-    print("." , end="" , flush=True)
+seconds = args.time
+print(f"starting timer of {seconds} seconds")
+s = ""
+for i in range(seconds):
+    s+="."
+print(s , end="\r" , flush=True)
+for i in range(seconds):
+    s = list(s)
+    for j in range(0,i+1):
+        s[j] = "#"
+    s = "".join(s)
+    print(s , end="\r" , flush=True)
     time.sleep(1)
 print ("\n Done !")
